9dca95d18bc9f1a066660bc928b8767f32ad88ac
marwensghair95 <marwensghair95@gmail.com>
2020-11-19 12:14:24 +0100

    AUTH TAKEN

--- src/app/nav-bar/nav-bar.component.ts
+++ src/app/nav-bar/nav-bar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../services/auth.service';
 
 @Component({
   selector: 'app-nav-bar',
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavBarComponent implements OnInit {
 
-  constructor() { }
+  constructor(private AuthServices: AuthService) { }
 
   ngOnInit(): void {
+  }
+  logOutBar(){
+    this.AuthServices.logout();
   }
 
 }
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -11,14 +11,30 @@ export class AuthService {
     const users = JSON.parse(localStorage.getItem("users")) || [];
     const test = users.find(use => use.email === user.email && use.password === user.password);
     if (test !== undefined) {
+      localStorage.setItem('token', 'JWT');
       return true;
     } else { 
       return false; 
     }
   }
+  
+  logout() : void {
+    localStorage.removeItem('token');
+  }
+
   register(user) {
     const users = JSON.parse(localStorage.getItem("users")) || [];
     users.push(user)
     localStorage.setItem('users', JSON.stringify(users));
+  }
+
+  public isAuthenticated(): boolean {
+    const token = localStorage.getItem('token');
+    if (token===null) {
+      return false;
+    }else{
+      return true;
+    }
+   
   }
 }
